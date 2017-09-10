5e2ff2e0b4025e00461343cbbe609db56ef294c4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2017-09-09 23:50:57 -0700

    Minor formatting fix

--- tests/PhpTest.php
+++ tests/PhpTest.php
@@ -5,7 +5,7 @@ use PHLAK\Config;
 class PhpTest extends PHPUnit_Framework_TestCase
 {
     use Initializable;
-    
+
     public function setUp()
     {
         $this->validConfig = __DIR__ . '/files/php/config.php';
